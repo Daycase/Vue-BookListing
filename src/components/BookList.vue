097951fb080b9e0f7b87e65fe123e569b5ea54e8
Matt Rose <matt.rose@outlook.com>
2019-06-18 11:18:00 +0100

    Next task

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,6 +1,9 @@
 <template>
 	<div>
 		<h1>{{title}}</h1>
+		<ul>
+			<li v-for="book in books">{{book.title}} - {{book.author}}</li>
+		</ul>
 	</div>
 </template>
 
@@ -9,7 +12,13 @@
 		name: 'BookList',
 		data() {
 			return {
-				title: 'All Books'
+				title: 'All Books',
+				books: [
+					{title: "Lord of the Rings", author: "JRR Tolkien"},
+					{title: "Hitchhikers Guide To The Galaxy", author: "Douglas Adams"},
+					{title: "Fear & Loathing in Las Vegas", author: "Hunter S. Thompson"}
+				]
+
 			}
 		}
 	}
@@ -18,5 +27,13 @@
 <style>
 	h1,h2 {
 		font-weight: normal;
+	}
+	ul {
+		list-style-type: none;
+		padding: 0;
+	}
+	li {
+		display: block;
+		margin: 0 10px;
 	}
 </style>
